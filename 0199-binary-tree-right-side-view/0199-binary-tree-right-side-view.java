cdb7c699789d3db8ca7b64ca2d503090e28eddc1
Srikanth Reddy Gunukula <154919226+srikanth-reddy-g@users.noreply.github.com>
2024-04-21 18:20:09 -0400

    Time: 0 ms (100.00%), Space: 42.2 MB (23.99%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -15,19 +15,35 @@
  */
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
-        Queue<TreeNode> q=new LinkedList<>();
+        // Recursive Solution T.C = O(n) and S.C = O(h) 
+        // where h = height of tree
         List<Integer> rView = new ArrayList<>();
-        if(root==null) return rView;
-        q.add(root);
-        while(!q.isEmpty()){
-            int qsize= q.size();
-            for(int i=0;i<qsize;i++){
-                TreeNode node=q.poll();
-                if(i==qsize-1) rView.add(node.val);
-                if(node.left!=null) q.add(node.left);
-                if(node.right!=null) q.add(node.right);
-            }
-        }
+        recursiveRightView(root, rView, 0);
         return rView;
     }
+    public void recursiveRightView(TreeNode root,List<Integer> rView,int level){
+        if(root==null)
+        return;
+        if(level==rView.size()) rView.add(root.val);
+        recursiveRightView(root.right, rView, level+1);
+        recursiveRightView(root.left, rView, level+1);
+    }
+
+
+        // Iterative method with T.C = O(n), S,C = O(n)
+        // Queue<TreeNode> q=new LinkedList<>();
+        // List<Integer> rView = new ArrayList<>();
+        // if(root==null) return rView;
+        // q.add(root);
+        // while(!q.isEmpty()){
+        //     int qsize= q.size();
+        //     for(int i=0;i<qsize;i++){
+        //         TreeNode node=q.poll();
+        //         if(i==qsize-1) rView.add(node.val);
+        //         if(node.left!=null) q.add(node.left);
+        //         if(node.right!=null) q.add(node.right);
+        //     }
+        // }
+        // return rView;
+    // }
 }
