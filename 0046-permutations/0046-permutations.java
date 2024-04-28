da7730a50588fc86ff709197cfcbf53c30915281
Srikanth Reddy Gunukula <154919226+srikanth-reddy-g@users.noreply.github.com>
2024-04-28 12:40:49 -0400

    Time: 2 ms (49.52%), Space: 44.6 MB (17.22%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,26 +1,20 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> answer = new ArrayList<>();
-        permutations(0,new ArrayList<>(),answer,nums);
+        permutations(new ArrayList<>(),answer,nums);
         return answer;
     }
-    public void permutations(int index,List<Integer> sublist,List<List<Integer>> answer,int nums[]){
-        if(index==nums.length){
+    public void permutations(List<Integer> sublist,List<List<Integer>> answer,int nums[]){
+        if(sublist.size()==nums.length){
             answer.add(new ArrayList<>(sublist));
             return;
         }
-        for(int i=index;i<nums.length;i++)
+        for(int i=0;i<nums.length;i++)
         {
-            swap(nums, index, i);
-            sublist.add(nums[index]);
-            permutations(index+1,sublist,answer,nums);
-            sublist.remove(sublist.size()-1);
-            swap(nums, index, i);
+            if(!sublist.contains(nums[i]))
+            {sublist.add(nums[i]);
+            permutations(sublist,answer,nums);
+            sublist.remove(sublist.size()-1);}
         }
-    }
-    public void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
     }
 }
