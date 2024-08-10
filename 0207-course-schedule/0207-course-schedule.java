c3f729584184cd36a627c42019a8bc05304bc6ec
Srikanth Reddy Gunukula <154919226+srikanth-reddy-g@users.noreply.github.com>
2024-08-10 06:00:57 +0530

    Time: 5 ms (66.88%), Space: 44.3 MB (82.41%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int indegree[] = new int[numCourses];
+        int M = prerequisites.length;
+        Queue<Integer> q = new LinkedList<>();
+        List<List<Integer>> list = new ArrayList<>();
+        int ans[] = new int[numCourses];
+        for (int i = 0; i < numCourses; i++) {
+            list.add(new ArrayList());
+        }
+        for (int i = 0; i < M; i++) {
+            indegree[prerequisites[i][0]]++;
+            list.get(prerequisites[i][1]).add(prerequisites[i][0]);
+        }
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0)
+                q.add(i);
+        }
+        int i = 0;
+        int cnt = 0;
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            for (Integer it : list.get(node)) {
+                indegree[it]--;
+                if (indegree[it] == 0)
+                    q.add(it);
+            }
+            ans[i++] = node;
+            cnt++;
+        }
+        return (cnt == numCourses) ? true : false;
+    }
+}
