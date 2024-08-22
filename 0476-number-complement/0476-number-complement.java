d16fd67c570b13a6b4ce073be3500898960574b8
Srikanth Reddy Gunukula <154919226+srikanth-reddy-g@users.noreply.github.com>
2024-08-23 02:20:34 +0530

    Time: 0 ms (100.00%), Space: 40.3 MB (67.46%) - LeetHub

---
+++ 0476-number-complement/0476-number-complement.java
@@ -0,0 +1,14 @@
+class Solution {
+  public int findComplement(int num) {
+    int todo = num, bit = 1;
+    while (todo != 0) {
+      // flip the current bit
+      num = num ^ bit;
+
+      // prepare for the next run
+      bit = bit << 1;
+      todo = todo >> 1;
+    }
+    return num;
+  }
+}
