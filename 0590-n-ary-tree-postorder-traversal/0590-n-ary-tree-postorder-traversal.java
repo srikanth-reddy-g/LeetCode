241493368820d2e6fbfa7ff4c4f9a49757b5c6c1
Srikanth Reddy Gunukula <154919226+srikanth-reddy-g@users.noreply.github.com>
2024-08-27 00:12:43 +0530

    Time: 4 ms (12.61%), Space: 44.6 MB (50.84%) - LeetHub

---
+++ 0590-n-ary-tree-postorder-traversal/0590-n-ary-tree-postorder-traversal.java
@@ -0,0 +1,44 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+
+    public List<Integer> postorder(Node root) {
+        List<Integer> result = new ArrayList<>();
+
+        // If the root is null, return the empty list
+        if (root == null) return result;
+
+        Stack<Node> stack = new Stack<>();
+        stack.add(root);
+
+        // Traverse the tree using the stack
+        while (!stack.isEmpty()) {
+            Node currentNode = stack.pop();
+
+            result.add(currentNode.val);
+            // Push all the children of the current node to the stack
+            for (Node child : currentNode.children) stack.add(child);
+        }
+
+        // Reverse the result list to get the correct postorder traversal
+        Collections.reverse(result);
+        return result;
+    }
+}
