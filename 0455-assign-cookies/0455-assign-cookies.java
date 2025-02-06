325e8b646d574be073aa2692d9df2eea593f20e6
Srikanth Reddy Gunukula <154919226+srikanth-reddy-g@users.noreply.github.com>
2025-02-05 23:39:22 -0500

    Time: 10 ms (81.4%), Space: 49 MB (15.62%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int result = 0;
+        int i = 0;
+        int j = 0;
+        for (; i < g.length && j < s.length;) {
+            if (s[j] >= g[i]) {
+                result++;
+                i++;
+            }
+            j++;
+        }
+
+        return result;
+
+    }
+}
