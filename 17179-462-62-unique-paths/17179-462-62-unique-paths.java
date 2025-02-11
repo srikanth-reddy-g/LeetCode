327e8ecd7a980094ad1f39b931b6b59a108d6811
Srikanth Reddy Gunukula <154919226+srikanth-reddy-g@users.noreply.github.com>
2025-02-11 16:45:00 -0500

    Time: 0 ms, Memory: 40.5 MB - LeetHub

--- 17179-462-62-unique-paths/17179-462-62-unique-paths.java
+++ 17179-462-62-unique-paths/17179-462-62-unique-paths.java
@@ -1,7 +1,18 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         int dp[][] = new int[m][n];
-        return findUniquePaths(m - 1, n - 1, m, n, dp);
+
+        // Tabulation
+        dp[0][0] = 1;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0)
+                    continue;
+                dp[i][j] = ((i - 1) >= 0 ? dp[i - 1][j] : 0) + ((j - 1) >= 0 ? dp[i][j - 1] : 0);
+            }
+        }
+        return dp[m - 1][n - 1];
+        // return findUniquePaths(m - 1, n - 1, m, n, dp);
     }
 
     // Memoization
